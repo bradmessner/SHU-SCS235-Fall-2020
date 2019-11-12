0c4a1dbf0b19092e87424475d79631f842f0a8f3
Brad D Messner <bmessner@setonhill.edu>
2019-11-11 23:30:22 -0500

    Updated Nov 11, 2019

--- files.py
+++ files.py
@@ -10,5 +10,6 @@ with open('/Users/bradmessner/Desktop/emp_data.csv', 'rb') as csvfile:
 # Write data out to a file
 with open('/Users/bradmessner/Desktop/emp_data_out.csv', 'wb') as csvfile:
     filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    filewriter.writerow(['Name'] + ['Office'] + ['Course'])
     filewriter.writerow(['Brad D Messner'] + ['Lynch 103'] + ['Machine Learning'])
     filewriter.writerow(['Ryan #1', 'Lynch 106', 'Machine Learning'])
